67d83852738ef7872e29e26039a18048cca87f39
Mario Pagnozzi <mario.antonio.pagnozzi30@gmail.com>
2026-09-13 21:18:58 -0300

    criação objeto cadastrar

--- app/cadastro.tsx
+++ app/cadastro.tsx
@@ -1,3 +1,4 @@
+import { router } from 'expo-router';
 import { useEffect, useState } from 'react';
 import {
   Pressable,
@@ -7,7 +8,6 @@ import {
   TextInput,
   View,
 } from 'react-native';
-import { router } from 'expo-router';
 
 export default function Cadastro() {
   const [nome, setNome] = useState('');
@@ -171,6 +171,16 @@ export default function Cadastro() {
         return data;
       }
 
+      const obj = new Object({
+        Nome: nome,
+        DataNascimento: formatarData(dataNascimento),
+        DataFalecimento: formatarData(dataFalecimento),
+        Cemiterio: cemiterio || null,
+        Quadra: quadra || null,
+        Lote: lote || null,
+        Latitude: latitude ? Number(latitude.replace(',', '.')) : null,
+        Longitude: longitude ? Number(longitude.replace(',', '.')) : null,
+      });
       const resposta = await fetch(
         'http://192.168.1.74:3000/falecidos',
         {
@@ -178,20 +188,7 @@ export default function Cadastro() {
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({
-            Nome: nome,
-            DataNascimento: formatarData(dataNascimento),
-            DataFalecimento: formatarData(dataFalecimento),
-            Cemiterio: cemiterio || null,
-            Quadra: quadra || null,
-            Lote: lote || null,
-            Latitude: latitude
-              ? Number(latitude.replace(',', '.'))
-              : null,
-            Longitude: longitude
-              ? Number(longitude.replace(',', '.'))
-              : null,
-          }),
+          body: JSON.parse(JSON.stringify(obj)),
         }
       );
 
